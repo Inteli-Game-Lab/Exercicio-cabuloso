01922c7a18031c2626777126b963f714cfe78001
ElgenneniF <Felipe.Elgenneni@sou.inteli.edu.br>
2025-02-24 22:59:34 -0300

    a

---
+++ scenes/tank.js
@@ -0,0 +1,20 @@
+class tank extends Phaser.Scene{
+    constructor(){
+            super({key: "tank"});
+        };
+    
+    preload(){
+       //assets
+        
+    };
+    
+    create(){
+        teclado = this.input.keyboard.createCursorKeys();
+    };
+   
+    update(){
+
+    };
+    
+
+};
